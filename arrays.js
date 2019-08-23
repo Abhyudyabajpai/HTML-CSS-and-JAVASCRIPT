9567184ef0022adc13977b8e76b72cc2595bd7c8
Abhyudya <abhyudyabajpai10@gmail.com>
2019-08-24 00:21:01 +0530

    arrays_in_javascript

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var list =["tiger","cat","horse","rabit"]; 
+console.log( list[2]);
+
+var list2 =[ 
+    ["car","motorcycle","trian"]
+]
+console.log(list2[0][1]);
+
+//list.pop(); {for removing the last element of the list}
+//list.shift(); {for removing the first element of the list}
+//list.push("text"); {for adding a new element to the list}
+//list.sort(); {for the sorting the list elements}
