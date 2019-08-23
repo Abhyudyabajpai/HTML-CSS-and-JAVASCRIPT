3e7eb9f66c9fcf7618f639f914a7deff57972742
Abhyudya <abhyudyabajpai10@gmail.com>
2019-08-23 23:00:35 +0530

    javascript_in_html

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-alert("you are smart!");
-console.log("Hello");
+function sayHello() {
+console.log("Hello !");
+}
+sayHello();
+
+var sayBye = function() {
+    console.log("Bye");
+}
+sayBye();
+
+function multiply(a,b) {
+   console.log(a*b);
+}
+multiply(3,6);
+multiply(7,8);
+
+
+function avg(a,b,c,d) {
+    return (a+b+c+d)/4
+}
