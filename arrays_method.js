9564818afcf5e9abec696ec10552ff555771d1db
Abhyudya <abhyudyabajpai10@gmail.com>
2019-08-24 11:34:43 +0530

    array_methods

---
+++ arrays_method.js
@@ -0,0 +1,5 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+alert(document.getElementById("demo").innerHTML = fruits.toString());
+
+var cars =["Ferrari","Bentley","Aston Martin","Rolls Royce"];
+alert(document.getElementById("wheels").innerHTML = cars.join("&"));
