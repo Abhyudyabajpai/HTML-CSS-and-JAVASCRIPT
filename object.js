6f166628e2b261ccc5f672c613cfa56841ec2c87
Abhyudya <abhyudyabajpai10@gmail.com>
2019-08-24 14:10:14 +0530

    object

--- object.js
+++ object.js
@@ -1,7 +1,26 @@
 var user = {
     name : "Tom",
-    age : "41" ,
+    age : 41,
     sex : "Male",
     Hobby : "Tennis",
 };
+var list = [
+   { username : "candice",
+    password : "@#12",
+}, 
+ {
+    username : "janice",
+    password : "yt74",
+}
+];
+// TO add a new character to user:
+// user.favfood = "Sushi";
 
+// We can have list inside an object as vice-versa.
+
+// user.kids :["john","max"]
+// user.kids[1]; {accessing list items inside object}
+// "max"
+ 
+// list[0].password; {accessing object items inside list}
+//"@#12"
