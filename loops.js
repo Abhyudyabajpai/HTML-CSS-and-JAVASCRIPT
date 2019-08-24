db84d81c63572c62507cada1210027ca8622987b
Abhyudya <abhyudyabajpai10@gmail.com>
2019-08-24 19:25:37 +0530

    loops

--- loops.js
+++ loops.js
@@ -5,7 +5,33 @@ var todos = [
     "study javascript",
     "eat healthy"
 ];
+todosLength = todos.length;
 
+// for loop
 for (var i=0;i < todos.length ; i++) {
     console.log(todos[i]+ "!");
 }
+ 
+//for (var i=0;i< todosLength; i++) {
+  //  todos.pop();
+//}
+
+//forEach loop
+
+todos.forEach(function(n,i){
+    console.log(n,i);
+})
+
+
+// while loop
+var counterOne = 0;
+while (counterOne < 10) {
+    console.log(counterOne);
+    counterOne++
+}
+// do while loop
+
+var count2 = 10;
+do { console.log(count2);
+    count2--;
+} while (count2 > 0);
