22d85a5f8511ac4fcc2c6626c6526f22b3d33af5
Abhyudya <abhyudyabajpai10@gmail.com>
2019-09-14 19:26:26 +0530

    functions_in_js

--- exercise7-SOLUTIONS.js
+++ exercise7-SOLUTIONS.js
@@ -1,31 +1,51 @@
-// Create an object and an array which we will use in our facebook exercise. 
 
-// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
-var data  = {
-	username: "andrei",
-	password : "supersecret",
+// For all of these, what is the value of a when the function gets called with the alert()
+// #1
+function q1() {
+    var a = 5;
+    if(a > 1) {
+        a = 3;
+    }
+    alert(a); //3
 }
 
-// 2. Create an array which contains the object you have made above and name the arry "database"
-var database = [
-	{
-		username: "andrei",
-		password: "supersecret",
-	}
-];
+//#2
+// you must first run q2() in your console to add the new value. then run q22()
+var a = 0;
+function q2() {
+    a = 5;
+}
 
-// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
-var newsfeed = [
-	{
-		username: "Bobby",
-		timeline: "So tired from all that learning!"
-	},
-	{
-		username: "Sally",
-		timeline: "Javascript is sooooo cool!"
-	},
-	{
-		username: "Mitch",
-		timeline: "Javascript is preeetyy cool!"
-	}
-];
+function q22() {
+    alert(a); //5
+}
+
+// run in the console:
+// q22() -- 0
+// q2() -- undefined
+// q22() -- 5
+
+//#3
+function q3() {
+    window.a = "hello";
+}
+
+// you must first run q3() in your console to add the a property to the window. then run q32()
+function q32() {
+    alert(a); //"hello"
+}
+
+//#4
+var a = 1;
+function q4() {
+    var a = "test";
+    alert(a); //"test"
+}
+
+//#5 with var keyword, if statements do not create a new scope
+var a = 2;
+if (true) {
+    var a = 5;
+    alert(a); // 5
+}
+alert(a); // 5
